2857fc428ff8ab9cd0467b760bd9935541a13312
Curtis Chen <curtischen1234@gmail.com>
2026-04-01 02:27:01 -0700

    Add umbreon

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,17 @@ import { ReverseHolo } from "@/components/ReverseHolo"
 export default function Page() {
   return (
     <div className="flex min-h-dvh flex-col items-center justify-center gap-20 bg-slate-950 py-20">
-      <SpecialIllustration
-        foilSrc="clefairyexfoil.webp"
-        cardSrc="clefairyex.webp"
-      />
-      <ReverseHolo foilSrc="tyrantrumfoil.webp" cardSrc="tyrantrum.png" />
+      <div className="mx-5 grid auto-rows-fr grid-cols-1 gap-15 lg:grid-cols-2">
+        <SpecialIllustration
+          foilSrc="clefairyexfoil.webp"
+          cardSrc="clefairyex.webp"
+        />
+        <SpecialIllustration
+          foilSrc="umbreonexfoil.webp"
+          cardSrc="umbreonex.png"
+        />
+        <ReverseHolo foilSrc="tyrantrumfoil.webp" cardSrc="tyrantrum.png" />
+      </div>
     </div>
   )
 }
