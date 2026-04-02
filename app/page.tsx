f532c7b839a99a0ad8cfd55c9f57cd2027e9952e
Curtis Chen <curtischen1234@gmail.com>
2026-04-02 02:03:22 -0700

    Add title description

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { ReverseHolo } from "@/components/ReverseHolo"
 export default function Page() {
   return (
     <div className="flex min-h-dvh flex-col items-center justify-center gap-20 bg-slate-950 py-20">
+      <h1 className="shimmer text-4xl font-bold text-gray-400 shimmer-color-amber-50 md:text-5xl lg:text-6xl">
+        Pokemon Cards
+      </h1>
       <div className="mx-5 grid auto-rows-fr grid-cols-1 gap-15 lg:grid-cols-2">
         <SpecialIllustration
           foilSrc="clefairyexfoil.webp"
